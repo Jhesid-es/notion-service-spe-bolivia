58a681c4075b589f8b84faeb9b5d2bd285b312de
jhesid2345 <jhesid2345@gmail.com>
2025-01-26 14:33:33 -0400

    feat: :rocket: Cambios en server

--- notion.js
+++ notion.js
@@ -2,7 +2,9 @@ const axios = require("axios");
 const express = require("express");
 
 const NOTION_TOKEN = "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";
-const DATABASE_ID = "1802d422723480acaa35ccb1b45f4d67";
+const databases = {
+  programas: "1802d422723480acaa35ccb1b45f4d67"
+}
 
 const headers = {
   Authorization: `Bearer ${NOTION_TOKEN}`,
@@ -10,53 +12,40 @@ const headers = {
   "Notion-Version": "2022-06-28",
 };
 
-const app = express();
-
-const createPage = async (data) => {
-  const createUrl = "https://api.notion.com/v1/pages";
-
-  const payload = {
-    parent: { database_id: DATABASE_ID },
-    properties: data,
-  };
-
+const obtenerProgramas = async (numPages = null) => {
+  const url = `https://api.notion.com/v1/databases/${databases.programas}/query`;
+  const getAll = numPages === null;
+  const pageSize = getAll ? 100 : numPages;
+  let results = [];
+  let hasMore = true;
+  let startCursor = null;
   try {
-    const res = await axios.post(createUrl, payload, { headers });
-    return res.data;
+    while (hasMore) {
+      const payload = {
+        page_size: pageSize,
+        ...(startCursor && { start_cursor: startCursor }),
+      };
+      const response = await axios.post(url, payload, { headers });
+      const data = response.data;
+      results = results.concat(data.results);
+      hasMore = data.has_more;
+      startCursor = data.next_cursor;
+      if (!getAll) break;
+    }
+    return results;
   } catch (error) {
     console.error(
-      "Error creating page:",
+      "Error fetching pages:",
       error.response?.data || error.message
     );
   }
 };
 
-const title = "Test Title";
-const description = "Test Description";
-const publishedDate = new Date().toISOString();
 
-const data = {
-  URL: {
-    title: [
-      {
-        text: { content: description },
-      },
-    ],
-  },
-  Titulo: {
-    rich_text: [
-      {
-        text: { content: title },
-      },
-    ],
-  },
-  Publicado: {
-    date: { start: publishedDate, end: null },
-  },
-};
+const app = express();
 
-app.get("/", async (req, res) => {
-  const pages = await getPages();
+app.get("/programas", async (req, res) => {
+  const pages = await obtenerProgramas();
   const datos = await pages.map((page) => {
     const props = page.properties;
     const title = props.title;
@@ -69,59 +58,12 @@ app.get("/", async (req, res) => {
       paragraph: paragraph.rich_text[0].text.content,
     };
   });
-  res.send(`Welcome spe bolivia, ${datos[0].title}`);
+  res.send(datos);
 });
-const getPages = async (numPages = null) => {
-  const url = `https://api.notion.com/v1/databases/${DATABASE_ID}/query`;
-  const getAll = numPages === null;
-  const pageSize = getAll ? 100 : numPages;
 
-  let results = [];
-  let hasMore = true;
-  let startCursor = null;
 
-  try {
-    while (hasMore) {
-      const payload = {
-        page_size: pageSize,
-        ...(startCursor && { start_cursor: startCursor }),
-      };
 
-      const response = await axios.post(url, payload, { headers });
-      const data = response.data;
-
-      results = results.concat(data.results);
-      hasMore = data.has_more;
-      startCursor = data.next_cursor;
-
-      if (!getAll) break;
-    }
-
-    return results;
-  } catch (error) {
-    console.error(
-      "Error fetching pages:",
-      error.response?.data || error.message
-    );
-  }
-};
-
-(async () => {
-  const pages = await getPages();
-  const datos = await pages.map((page) => {
-    const props = page.properties;
-    const title = props.title;
-    const image = props.imagenes;
-    const paragraph = props.paragraph;
-    return {
-      title: title.title[0].text.content,
-      image: image.files[0].file.url,
-      paragraph: paragraph.rich_text[0].text.content,
-    };
-  });
-  console.log(datos);
-})();
 
 app.listen(3000, () => {
-  console.log("Server escuchando");
+  console.log("Servicio en funcionamiento");
 });

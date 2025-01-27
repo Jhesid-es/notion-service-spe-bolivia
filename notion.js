10d4bbcf9135200fe7fcba6c26ba53d160dccb92
jhesid2345 <jhesid2345@gmail.com>
2025-01-27 09:19:31 -0400

    feat: :rocket: Se adiciono nuevo endpoint

--- notion.js
+++ notion.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const NOTION_TOKEN = "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";
 const databases = {
   programas: "1802d422723480acaa35ccb1b45f4d67",
+  patrocinadores: "1872d4227234802e8780f29bf83a8d4d",
 };
 
 const headers = {
@@ -42,6 +43,35 @@ const obtenerProgramas = async (numPages = null) => {
   }
 };
 
+const obtenerPatrocinadores = async (numPages = null) => {
+  const url = `https://api.notion.com/v1/databases/${databases.patrocinadores}/query`;
+  const getAll = numPages === null;
+  const pageSize = getAll ? 100 : numPages;
+  let results = [];
+  let hasMore = true;
+  let startCursor = null;
+  try {
+    while (hasMore) {
+      const payload = {
+        page_size: pageSize,
+        ...(startCursor && { start_cursor: startCursor }),
+      };
+      const response = await axios.post(url, payload, { headers });
+      const data = response.data;
+      results = results.concat(data.results);
+      hasMore = data.has_more;
+      startCursor = data.next_cursor;
+      if (!getAll) break;
+    }
+    return results;
+  } catch (error) {
+    console.error(
+      "Error fetching pages:",
+      error.response?.data || error.message
+    );
+  }
+};
+
 const app = express();
 app.use(cors());
 
@@ -52,13 +82,30 @@ app.get("/programas", async (req, res) => {
     const title = props.title;
     const image = props.imagenes;
     const paragraph = props.paragraph;
-    console.log("leyedo datos de notion");
+    console.log("Obteniendo programas..");
     return {
       title: title?.title[0]?.text?.content ? title.title[0].text.content : "",
       image: image?.files[0]?.file?.url ? image.files[0].file.url : "",
       paragraph: paragraph?.rich_text[0]?.text?.content
         ? paragraph.rich_text[0].text.content
         : "",
+    };
+  });
+  res.send(datos);
+});
+
+app.get("/patrocinadores", async (req, res) => {
+  const pages = await obtenerPatrocinadores();
+  const datos = await pages.map((page) => {
+    const props = page.properties;
+    const nombre = props.nombre;
+    const image = props.logo;
+    console.log("Obteniendo patrocinadores..");
+    return {
+      nombre: nombre?.title[0]?.text?.content
+        ? nombre.title[0].text.content
+        : "",
+      logo: image?.files[0]?.file?.url ? image.files[0].file.url : "",
     };
   });
   res.send(datos);
